876e57ba288d985418bb339cc568ccafa2740071
mohammed <mohammed>
2019-07-17 08:52:38 +0100

    Completed my Profile Activity Button in Main Activity navigates to my Profile Activity

--- app/src/main/java/it/wemake/alc4phase1challenge/MainActivity.kt
+++ app/src/main/java/it/wemake/alc4phase1challenge/MainActivity.kt
@@ -2,6 +2,7 @@ package it.wemake.alc4phase1challenge
 
 import android.content.Context
 import android.content.Intent
+import android.os.Build.VERSION_CODES.M
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -14,9 +15,15 @@ class MainActivity : AppCompatActivity() {
 
         val context = this as Context
         val gotoAboutALCBTN = findViewById<Button>(R.id.btn_about_alc)
+        val gotoMyProfileBTN = findViewById<Button>(R.id.btn_my_profile)
 
         gotoAboutALCBTN.setOnClickListener {
             val intent = Intent(context, AboutALCActivity::class.java)
+            startActivity(intent)
+        }
+
+        gotoMyProfileBTN.setOnClickListener {
+            val intent = Intent(context, MyProfileActivity::class.java)
             startActivity(intent)
         }
 
---
+++ app/src/main/java/it/wemake/alc4phase1challenge/MyProfileActivity.kt
@@ -0,0 +1,12 @@
+package it.wemake.alc4phase1challenge
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MyProfileActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_my_profile)
+    }
+}
